5ed42f2e4ab4f53448ac7d028fce18b4632b250f
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-01-28 18:19:03 +0100

    Add functions to plot images and graphs as well as to compute the RGB mean on the training datasets

--- Modules/utilities.py
+++ Modules/utilities.py
@@ -4,6 +4,11 @@ from tensorflow import keras
 from Modules.config import *
 import shutil
 from pathlib import Path
+import numpy as np
+from Modules.pre_processing import images_dataset
+from matplotlib import pyplot as plt
+from tensorflow.image import psnr
+import seaborn as sn
 
 
 def download_datasets(extract=True):
@@ -68,4 +73,63 @@ def split_dataset(test_size=100):
                 shutil.move(os.path.join(train_folder, image), test_folder)
 
 
+def compute_dataset_mean():
+    folders = ['Train_A', 'Train_B', 'Train_HR']
+    for folder in folders:
+        path = os.path.join(base_dir, folder)
+        images_list = [os.path.join(path, item) for item in os.listdir(path)]
+        data_set = images_dataset(images_list)
+        mean_list = []
+        for image in data_set:
+            mean_list.append(np.mean(image, axis=(0, 1)))
+        print(np.mean(mean_list, axis=0))
 
+
+def plot_lr_hr_pair(lr_image, hr_image, ax=True, title=True):
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    axes[0].imshow(lr_image)
+    axes[1].imshow(hr_image)
+    if title:
+        axes[0].set_title('Low Resolution')
+        axes[1].set_title('High Resolution')
+    if not ax:
+        axes[0].axis('off')
+        axes[1].axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+def psnr_metric(true_img, pred_img):
+    return psnr(true_img, pred_img, max_val=1)
+
+
+def plot_history(metric, val_metric, loss, val_loss, title=None):
+    """
+    Plots the history of the training phase and validation phase. It compares in two different subplots the metric
+    and the loss of the model.
+
+    :param metric: list of values for every epoch.
+    :param val_metric: list of values for every epoch.
+    :param loss: list of values for every epoch.
+    :param val_loss: list of values for every epoch.
+    :param title: tile of the figure printed. default_value=None
+    :return:
+    """
+    sn.set()
+    fig, axes = plt.subplots(2, 1, sharex='all', figsize=(13, 8))
+    x_axis = list(range(1, len(metric) + 1))
+    # First subplot
+    axes[0].plot(x_axis, metric)
+    axes[0].plot(x_axis, val_metric)
+    axes[0].set(ylabel='PSNR')
+    axes[0].legend(['Train', 'Valid'], loc='lower right')
+    # Second subplot
+    axes[1].plot(x_axis, loss)
+    axes[1].plot(x_axis, val_loss)
+    axes[1].set(ylabel='Loss', xlabel='Epoch')
+    # Legend
+    axes[1].legend(['Train', 'Valid'], loc='upper right')
+    if title is not None:
+        fig.suptitle(title)
+    plt.tight_layout()
+    plt.show()
