d15c9e5a1b4791019b09603416428797ca7105f4
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-01-28 18:17:53 +0100

    Add function to prepare batches

--- test.py
+++ test.py
@@ -1,24 +1,44 @@
 # Import packages
 from Modules.utilities import download_datasets, split_dataset
+from Modules.pre_processing import prepare_batches
+import tensorflow as tf
+from A.a import A
 
+tf.compat.v1.enable_eager_execution()
+# set_memory_growth() allocates exclusively the GPU memory needed
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(physical_devices))
+if len(physical_devices) is not 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# ======================================================================================================================
 # Download dataset
 download_datasets()
-split_dataset(test_size=100)
+split_dataset(test_size=20)
+crop_size = 96
 
-# # ====================================================================================================================
-# # Data preprocessing
-# data_train, data_val, data_test = data_preprocessing(args...)
-# # ====================================================================================================================
-# # Task A
-# # Build model object.
-# model_A = A(args...)
-# # Train model based on the training set (you should fine-tune your model based on validation set).
-# acc_A_train, acc_A_valid = model_A.train(args...)
+# ======================================================================================================================
+# Data preprocessing
+train_batches, valid_batches, test_batches = prepare_batches(crop_size=crop_size, batch_size=5, task='A',
+                                                             rotation=True, flip=True)
+
+# ====================================================================================================================
+# Task A
+input_shape = [crop_size, crop_size, 3]
+# Build model object.
+model_A = A(input_shape)
+# Train model based on the training set (you should fine-tune your model based on validation set).
+acc_A_train, acc_A_valid = model_A.train(train_batches, valid_batches, epochs=5, verbose=1)
 # # Test model based on the test set.
 # acc_A_test = model_A.test(args...)
 # # Some code to free memory if necessary.
 # Clean up memory/GPU etc...
-#
+
+# ======================================================================================================================
+# Data preprocessing
+# train_batches, valid_batches, test_batches = prepare_batches(crop_size=96, batch_size=10, task='B',
+#                                                              rotation=True, flip=True)
+
 # # ====================================================================================================================
 # # Task B
 # # Build model object.
