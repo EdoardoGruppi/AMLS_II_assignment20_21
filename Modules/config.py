90f98eae36f51a96b5dac0d9d37a71f5b3ba74a7
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-01-28 18:20:03 +0100

    Add mean values computed on the training datasets

--- Modules/config.py
+++ Modules/config.py
@@ -4,3 +4,9 @@
 
 base_dir = './Datasets'
 dataset_url = 'https://data.vision.ee.ethz.ch/cvl/DIV2K/'
+# Channel-wise Mean computed on the entire training DIV2K dataset. The mean is computed via the compute_dataset_mean()
+# function within the module called utilities.
+RGB_MEAN_A = [114.3569244, 111.5630249, 103.15689794]
+RGB_MEAN_B = [114.33866302, 111.52755002, 103.09925148]
+RGB_MEAN_HR = [114.35629928, 111.561547, 103.1545782]
+
